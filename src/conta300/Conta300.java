415a8ae7abc0310f26d8d80b5e24fd86069d1b2c
Edgar Said Mesino <Edgar Said Mesino@DESKTOP-H416AU4>
2021-04-07 23:53:51 -0500

    pruebaa

--- src/conta300/Conta300.java
+++ src/conta300/Conta300.java
@@ -4,6 +4,7 @@ public class Conta300 {
 
     public static void main(String[] args) {
         System.out.println("Oppai o nagaiki sa seru");
+        System.out.println("Soy mesino no me sale ;,v ");
     }
     
 }
